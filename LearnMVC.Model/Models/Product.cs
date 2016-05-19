6ef939578e06d6fad57491194fbd7bf38f4438b5
Nhật Thái Quốc <Nht Thi Quc>
2016-05-19 14:30:05 +0700

    Create Database with Entity Framework Code First

--- LearnMVC.Model/Models/Product.cs
+++ LearnMVC.Model/Models/Product.cs
@@ -16,16 +16,25 @@ namespace LearnMVC.Model.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
         [Required]
+        [MaxLength(256)]
         public string Name { get; set; }
+
         [Required]
+        [MaxLength(256)]
         public string Alias { get; set; }
         public int CategoryID { get; set; }
         public string Image { get; set; }
-        public XElement MoreImage { get; set; }
+
+        [Column(TypeName ="xml")]
+        public string MoreImage { get; set; }
         public decimal Price { get; set; }
         public decimal? PromotionPrice { get; set; }
         public int? Warranty { get; set; }
+
+        [MaxLength(500)]
+        public string Description { set; get; }
         public string Content { get; set; }
         public bool? HomeFlag { get; set; }
         public bool? HotFlag { get; set; }
