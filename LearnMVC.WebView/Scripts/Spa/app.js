817386a5c808ec7facf0ac81cc8a8e32d46fb05f
Nhật Thái Quốc <thaiquocnhat96@gmail.com>
2016-06-27 09:56:22 +0700

    Done Toturial Directive and Service

--- LearnMVC.WebView/Scripts/Spa/app.js
+++ LearnMVC.WebView/Scripts/Spa/app.js
@@ -3,9 +3,38 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller("myController", myController);
-myController.$inject = ['$scope'];
+
+myApp.directive("learnDirective", learnDirective);
+
+myApp.service('validatorService', validatorService);
+
+myController.$inject = ["$scope", 'validatorService'];
+
 
 //declare
-function myController($scope) {
-    $scope.message = "this is a message";
+debugger;
+function myController($scope, validatorService) {
+
+    $scope.message = function () {
+        $scope.message = validatorService.checkNumber($scope.number);
+    }
+}
+
+function validatorService($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0)
+            return 'This is even';
+        else
+            return 'This is odd';
+    }
+}
+
+function learnDirective() {
+    return {
+        restrict:"A",
+        template: "<h1>This is the first custom directive</h1>"
+    }
 }
