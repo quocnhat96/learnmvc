c2eafe5c8a30b995fca7bdf77a5f3ece5fb635c8
Nhật Thái Quốc <thaiquocnhat96@gmail.com>
2016-06-28 10:55:39 +0700

    Route implementation for application

---
+++ LearnMVC.WebView/App/Components/products/products.module.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="C:\Users\thaiq\Documents\Visual Studio 2015\Projects\SourceCode\Git\LearnMVC.WebView\Assets/Admin/libs/angular/angular.js" />
+(function () {
+    angular.module("learnMVC.products", ['learnMVC.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/App/Components/products/productListView.html",
+            controller: "productListController"
+        }).state('products_add', {
+            url: "/products_add",
+            templateUrl: "/App/Components/products/productAddView.html",
+            controller: "productAddController"
+    })
+    }
+})();
--- LearnMVC.WebView/App/app.js
+++ LearnMVC.WebView/App/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="C:\Users\thaiq\Documents\Visual Studio 2015\Projects\SourceCode\Git\LearnMVC.WebView\Assets/Admin/libs/angular/angular.js" />
+(function () {
+    angular.module("learnMVC", ['learnMVC.products','learnMVC.common']).config(config);
+    
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('Home', {
+            url: "/Admin",
+            templateUrl: "/App/Components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/Admin');
+    }
+})();
